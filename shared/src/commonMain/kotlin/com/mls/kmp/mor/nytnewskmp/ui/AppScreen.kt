bb33e3fb3e40f4268f2465e6053e9b4f599cf9a6
Mor Shaar <morsrs.dev@gmail.com>
2023-10-01 16:08:09 +0300

    navigation: Add Navigation Rail support for larger screens sizes

--- shared/src/commonMain/kotlin/com/mls/kmp/mor/nytnewskmp/ui/AppScreen.kt
+++ shared/src/commonMain/kotlin/com/mls/kmp/mor/nytnewskmp/ui/AppScreen.kt
@@ -1,9 +1,17 @@
 package com.mls.kmp.mor.nytnewskmp.ui
 
 import HomeTab
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmarks
 import androidx.compose.material.icons.filled.Home
@@ -15,8 +23,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.core.screen.Screen
@@ -29,17 +45,38 @@ import com.mls.kmp.mor.nytnewskmp.ui.search.SearchTabRoute
 
 class AppScreen : Screen {
 
+    @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
     @Composable
     override fun Content() {
+        val windowSizeClass = calculateWindowSizeClass()
+        val shouldShowNavigationBar by remember(windowSizeClass) {
+            mutableStateOf(windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact)
+        }
+
         TabNavigator(HomeTab) {
             Scaffold(
-                content = {
-                    Box(modifier = Modifier.padding(it)) {
+                content = { innerPadding ->
+                    Row(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(innerPadding)
+                            .consumeWindowInsets(innerPadding)
+                            .windowInsetsPadding(
+                                WindowInsets.safeDrawing.only(
+                                    WindowInsetsSides.Horizontal,
+                                ),
+                            ),
+                    ) {
+
+                        if (!shouldShowNavigationBar) {
+                            NytNavigationRail()
+                        }
                         CurrentTab()
                     }
                 },
                 bottomBar = {
-                    NytNavigationBar()
+                    if (shouldShowNavigationBar) {
+                        NytNavigationBar()
+                    }
                 }
             )
         }
@@ -51,17 +88,42 @@ private fun NytNavigationBar() {
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.background,
     ) {
-        TabNavigationItem(HomeTab)
-        TabNavigationItem(SearchTabRoute)
-        TabNavigationItem(BookmarksTabRoute)
+        TabNavigationBarItem(HomeTab)
+        TabNavigationBarItem(SearchTabRoute)
+        TabNavigationBarItem(BookmarksTabRoute)
     }
 }
 
 @Composable
-private fun RowScope.TabNavigationItem(tab: Tab) {
+private fun RowScope.TabNavigationBarItem(tab: Tab) {
     val tabNavigator = LocalTabNavigator.current
     val selected = tabNavigator.current.key == tab.key
     NavigationBarItem(
+        selected = selected,
+        onClick = { tabNavigator.current = tab },
+        icon = {
+            Icon(
+                imageVector = getIconForTab(tab, selected),
+                contentDescription = tab.options.title
+            )
+        }
+    )
+}
+
+@Composable
+private fun NytNavigationRail() {
+    NavigationRail {
+        TabNavigationRailItem(HomeTab)
+        TabNavigationRailItem(SearchTabRoute)
+        TabNavigationRailItem(BookmarksTabRoute)
+    }
+}
+
+@Composable
+private fun TabNavigationRailItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+    val selected = tabNavigator.current.key == tab.key
+    NavigationRailItem(
         selected = selected,
         onClick = { tabNavigator.current = tab },
         icon = {
