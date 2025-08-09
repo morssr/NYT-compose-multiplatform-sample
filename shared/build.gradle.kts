099e329d40e7d2166fa4ec8cb3de0cf1b39a6e7d
Mor Shaar <morsrs.dev@gmail.com>
2025-08-09 18:30:15 +0300

    release: v2.0.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -115,8 +115,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
+        applicationId = "com.mls.mor.nytnews"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 8
+        versionName = "2.0.0"
     }
 
     compileOptions {
